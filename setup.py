df3a4e44a53824d0b63dc64582d8697c6c455bf6
Lydia Nemec <Lydia.Nemec@gmail.com>
2019-08-11 19:36:32 +0200

    Add the option to build the sphinx documentation to setup.py

--- setup.py
+++ setup.py
@@ -5,37 +5,49 @@
 #    hope that it will be useful, but WITHOUT ANY WARRANTY;
 #    without even the implied warranty of MERCHANTABILITY
 #    or FITNESS FOR A PARTICULAR PURPOSE.
-"""Setting up the How-To-Document-Data-Science-Projects module.
+"""
+Setting up the How-To-Document-Data-Science-Projects module.
+
+Example of how-to document a Data-Science project using
+Microsoft Azure Services.
 
 See: README.rst
 """
 
 import sys
-
 from setuptools import setup, find_packages
+from sphinx.setup_command import BuildDoc
 from example_mnist import __version__, __author__
 from os import path
 
 if sys.version_info < (3, 6):
     sys.exit('Sorry, Python < 3.6 is not supported.')
 
+project_name='How-To-Document-Data-Science-Projects'
 here = path.abspath(path.dirname(__file__))
 
 # Get long description from the README file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
+cmdclass = {'build_sphinx': BuildDoc}
 
-setup(name='How-To-Document-Data-Science-Projects',
+setup(name=project_name,
       version=__version__,
       author=__author__,
-      author_email='Lydia.Nemec@gmail.com',
-      maintainer='Dr. Lydia Nemec <Lydia.Nemec@gmail.com>',
+      maintainer= __author__,
       description='Example of how-to document a Data-Science project using Microsoft Azure Services.',
       long_description=long_description,
       long_description_content_type='text/markdown',
-      license='',
+      license='Apache License Version 2.0',
       url='https://github.com/lnemec/How-To-Document-Data-Science-Projects/edit/master',
       packages=find_packages(exclude=['doc']),
-      requires=['tensorflow', 'numpy']
+      package_data = { '': ['*.txt', '*.rst'] },
+      requires=['tensorflow', 'numpy'],
+      cmdclass=cmdclass,
+      command_options={'build_sphinx': {
+            'project': ('setup.py', project_name),
+            'version': ('setup.py', __version__),
+            'source_dir': ('setup.py', 'doc/source'),
+            'build_dir': ('setup.py', 'doc/build')} }
       )
