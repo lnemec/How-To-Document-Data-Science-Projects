11f3875a0d6aa87c66f9867d41907d54f979caf8
Lydia Nemec <Lydia.Nemec@gmail.com>
2019-08-11 19:35:07 +0200

    Add current directory to path

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -12,8 +12,7 @@
 #
 import os
 import sys
-#sys.path.append(os.path.abspath('../../How-To-Document-Data-Science-Projects'))
-sys.path.insert(0, os.path.abspath('../../How-To-Document-Data-Science-Projects'))
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
